7110e7722ec756ee1d0850829baa06d967c3cae4
lei yi <leiyisoe@gmail.com>
2019-06-03 15:27:44 +0630

    add MyClass

---
+++ src/com/java/plyd/MyClass.java
@@ -0,0 +1,20 @@
+package com.java.plyd;
+
+public class MyClass {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public MyClass(String name) {
+	super();
+	this.name = name;
+}
+
+
+}
